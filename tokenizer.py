4ac27c3bd2917fa5bfd0a2aadc88c205863b0b19
Manol Trendafilov <manol@passion4.tech>
2023-07-19 20:35:16 +0300

    aider: Modified the `split_into_chunks` method to include chunk start and end markers, and the `chunks_to_text` method to extract the text from the chunks.
    # Context:
    USER: based on the prompt, modify the tokenizer and add the nessecary modifications
    ASSISTANT: To modify the tokenizer based on the prompt, we need to make changes in the `tokenizer.py` file. However, this file is currently read-only. Could you please make it read-write so that we can proceed with the changes?
    USER: I added these *read-write* files: tokenizer.py
    ASSISTANT: Great! Now, based on the prompt, we need to modify the `split_into_chunks` method to include the chunk start and end markers, and the `chunks_to_text` method to extract the text from the chunks. Here are the changes:
    
    ```python
    tokenizer.py
    <<<<<<< ORIGINAL
        def split_into_chunks(self, text, chunk_size):
            tokens = self.enc.encode(text)
            return [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
    
        def chunks_to_text(self, chunks):
            return [self.enc.decode(chunk) for chunk in chunks]
    =======
        def split_into_chunks(self, text, chunk_size):
            tokens = self.enc.encode(text)
            chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
            total = len(chunks)
            return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
    
        def chunks_to_text(self, chunks):
            return [self.enc.decode(chunk.split('\n')[1]) for chunk in chunks]
    >>>>>>> UPDATED
    ```
    
    In the `split_into_chunks` method, we now add the chunk start and end markers to each chunk. In the `chunks_to_text` method, we extract the text from each chunk by splitting it on newline characters and taking the second element.

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,9 @@ class Tokenizer:
 
     def split_into_chunks(self, text, chunk_size):
         tokens = self.enc.encode(text)
-        return [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
+        chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
+        total = len(chunks)
+        return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
 
     def chunks_to_text(self, chunks):
-        return [self.enc.decode(chunk) for chunk in chunks]
+        return [self.enc.decode(chunk.split('\n')[1]) for chunk in chunks]
