f6d8434a36f7d1181f831b4d70391bc43bad3092
Manol Trendafilov <manol@passion4.tech>
2023-07-19 21:40:12 +0300

    aider: Added PyPDF2 library to requirements.txt and implemented PDFHandler class in file_handler.py to read PDF files. Modified app.py to use the new file_handler.py and added support for PDF files.
    # Context:
    USER: I added these *read-write* files: requirements.txt
    ASSISTANT: Great! Now, let's add the `PyPDF2` library to your `requirements.txt` file. This library will be used to read PDF files.
    
    ```python
    requirements.txt
    <<<<<<< ORIGINAL
    
    =======
    PyPDF2
    >>>>>>> UPDATED
    ```
    
    Next, we'll create a new file `file_handler.py` to handle different file types. This will follow the Open-Closed Principle of SOLID principles, allowing us to add support for more file types in the future without modifying existing code.
    
    ```python
    new_file/file_handler.py
    <<<<<<< ORIGINAL
    
    =======
    class FileHandler:
        def read(self, file):
            raise NotImplementedError
    
    class PDFHandler(FileHandler):
        def __init__(self):
            import PyPDF2
    
        def read(self, file):
            reader = PyPDF2.PdfFileReader(file)
            content = ""
            for page in range(reader.getNumPages()):
                content += reader.getPage(page).extractText()
            return content
    >>>>>>> UPDATED
    ```
    
    Finally, we'll modify `app.py` to use the new `file_handler.py`. We'll create a dictionary mapping file extensions to their respective handlers, and use this to read the uploaded file.
    
    ```python
    app.py
    <<<<<<< ORIGINAL
    uploaded_file = st.file_uploader("Upload file", type=['txt', 'pdf', 'docx', 'doc', 'xml'])
    file_content = None
    if uploaded_file is not None:
        file_content = uploaded_file.read().decode()
    =======
    from file_handler import PDFHandler
    
    file_handlers = {
        'pdf': PDFHandler(),
        # Add more handlers here as needed
    }
    
    uploaded_file = st.file_uploader("Upload file", type=list(file_handlers.keys()))
    file_content = None
    if uploaded_file is not None:
        file_extension = uploaded_file.name.split('.')[-1]
        handler = file_handlers.get(file_extension)
        if handler is not None:
            file_content = handler.read(uploaded_file)
        else:
            st.error(f'Unsupported file type: {file_extension}')
    >>>>>>> UPDATED
    ```
    
    Please let me know if you need further changes.

--- new_file/file_handler.py
+++ new_file/file_handler.py
@@ -12,3 +12,17 @@ class PDFHandler(FileHandler):
         for page in range(reader.getNumPages()):
             content += reader.getPage(page).extractText()
         return content
+class FileHandler:
+    def read(self, file):
+        raise NotImplementedError
+
+class PDFHandler(FileHandler):
+    def __init__(self):
+        import PyPDF2
+
+    def read(self, file):
+        reader = PyPDF2.PdfFileReader(file)
+        content = ""
+        for page in range(reader.getNumPages()):
+            content += reader.getPage(page).extractText()
+        return content
