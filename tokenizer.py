feab9b12794f1fd7341693dc9c5c0605aab5652a
Manol Trendafilov <manol@passion4.tech>
2023-07-19 20:45:27 +0300

    aider: Modified the `split_into_chunks` method to return a list of strings by joining the tokens in each chunk.
    # Context:
    USER: convert the list of tokens to strings
    ASSISTANT: To convert the list of tokens to strings, we need to modify the `split_into_chunks` method in the `tokenizer.py` file. Currently, the method is returning a list of tokens. We need to change it to return a list of strings.
    
    Here is the change:
    
    ```python
    tokenizer.py
    <<<<<<< ORIGINAL
        def split_into_chunks(self, text, chunk_size, prompt):
            tokens = self.enc.encode(text)
            chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
            total = len(chunks)
            chunks[0] = prompt + str(chunks[0])
            return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
    =======
        def split_into_chunks(self, text, chunk_size, prompt):
            tokens = self.enc.encode(text)
            chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
            total = len(chunks)
            chunks[0] = prompt + str(chunks[0])
            return [f'[START CHUNK {i+1}/{total}]\n{" ".join(map(str, chunk))}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
    >>>>>>> UPDATED
    ```
    
    In the updated code, we use the `join` function to convert the list of tokens in each chunk into a string. The `map` function is used to convert each token into a string before joining them.

--- tokenizer.py
+++ tokenizer.py
@@ -10,7 +10,7 @@ class Tokenizer:
         chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
         total = len(chunks)
         chunks[0] = prompt + str(chunks[0])
-        return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
+        return [f'[START CHUNK {i+1}/{total}]\n{" ".join(map(str, chunk))}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
 
     def chunks_to_text(self, chunks):
         return [self.enc.decode(chunk.split('\n')[1]) for chunk in chunks]
