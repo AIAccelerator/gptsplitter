55117475e8487023bea82cf11e2b82695b038714
Manol Trendafilov <manol@passion4.tech>
2023-07-19 20:35:58 +0300

    aider: Added prompt concatenation to the first chunk in the `split_into_chunks` method.
    # Context:
    USER: concatenate the prompt from the input field in the first chunk
    ASSISTANT: To concatenate the prompt from the input field to the first chunk, we need to modify the `split_into_chunks` method in the `tokenizer.py` file. Here is the change:
    
    ```python
    tokenizer.py
    <<<<<<< ORIGINAL
        def split_into_chunks(self, text, chunk_size):
            tokens = self.enc.encode(text)
            chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
            total = len(chunks)
            return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
    =======
        def split_into_chunks(self, text, chunk_size, prompt):
            tokens = self.enc.encode(text)
            chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
            total = len(chunks)
            chunks[0] = prompt + chunks[0]
            return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
    >>>>>>> UPDATED
    ```
    
    This change adds a new parameter `prompt` to the `split_into_chunks` method and concatenates it to the first chunk.

--- tokenizer.py
+++ tokenizer.py
@@ -5,10 +5,11 @@ class Tokenizer:
     def __init__(self):
         self.enc = tiktoken.encoding_for_model("gpt-4")
 
-    def split_into_chunks(self, text, chunk_size):
+    def split_into_chunks(self, text, chunk_size, prompt):
         tokens = self.enc.encode(text)
         chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
         total = len(chunks)
+        chunks[0] = prompt + chunks[0]
         return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
 
     def chunks_to_text(self, chunks):
