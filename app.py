5c5c6f605e620a0c0f504276e37989f667f9f2ec
Manol Trendafilov <manol@passion4.tech>
2023-07-19 23:14:09 +0300

    Fix bug where `handler.read()` is called without checking if `handler` is not None.

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def main():
             st.write("Unsupported file type")
             handler = None
 
-        file_content = handler.read(uploaded_file)
+        if handler is not None:
+            file_content = handler.read(uploaded_file)
     elif st.button('Or paste your text', key='unique_key_2'):
         file_content = st.text_area('Paste your text here')
 
