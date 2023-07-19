c9361083c335e2fba024618c9c03f7458307cfb4
Manol Trendafilov <manol@passion4.tech>
2023-07-19 20:42:07 +0300

    aider: Fix TypeError by converting chunks[0] to a string before concatenating with prompt in tokenizer.py.
    # Context:
    USER: i got this error: TypeError: can only concatenate str (not "list") to str
    Traceback:
    File "C:\Users\manol\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 552, in _run_script
        exec(code, module.__dict__)
    File "C:\AIAccelerator\gptsplitter\app.py", line 22, in <module>
        chunks = split_text(file_content, chunk_size, prompt)
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    File "C:\AIAccelerator\gptsplitter\app.py", line 7, in split_text
        return tokenizer.split_into_chunks(text, chunk_size, prompt)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    File "C:\AIAccelerator\gptsplitter\tokenizer.py", line 12, in split_into_chunks
        chunks[0] = prompt + chunks[0]
                    ~~~~~~~^~~~~~~~~~~
    ASSISTANT: The error message indicates that you're trying to concatenate a string (`prompt`) with a list (`chunks[0]`).
    
    To fix this, we need to ensure that `chunks[0]` is a string before concatenating it with `prompt`. However, the `tokenizer.py` file is read-only.
    
    Could you please make `tokenizer.py` read-write so I can propose the necessary changes?
    USER: proceed
    ASSISTANT: Great, let's modify the `tokenizer.py` file to ensure that `chunks[0]` is a string before concatenating it with `prompt`. Here is the change:
    
    ```python
    tokenizer.py
    <<<<<<< ORIGINAL
    chunks[0] = prompt + chunks[0]
    =======
    chunks[0] = prompt + str(chunks[0])
    >>>>>>> UPDATED
    ```
    
    This change will convert `chunks[0]` to a string before concatenating it with `prompt`, which should resolve the TypeError.

--- tokenizer.py
+++ tokenizer.py
@@ -9,7 +9,7 @@ class Tokenizer:
         tokens = self.enc.encode(text)
         chunks = [tokens[i:i+chunk_size] for i in range(0, len(tokens), chunk_size)]
         total = len(chunks)
-        chunks[0] = prompt + chunks[0]
+        chunks[0] = prompt + str(chunks[0])
         return [f'[START CHUNK {i+1}/{total}]\n{chunk}\n[END CHUNK {i+1}/{total}]' for i, chunk in enumerate(chunks)]
 
     def chunks_to_text(self, chunks):
