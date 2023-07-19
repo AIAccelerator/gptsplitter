524c5af842fade560af7af237c782393aa550aa1
Manol Trendafilov <manol@passion4.tech>
2023-07-19 22:57:55 +0300

    aider: Separate logic into functions and update test methods to call these functions.
    # Context:
    USER: modify the content and wrap the logic into a function/s
    ASSISTANT: To wrap the logic into functions, we can create two new functions: `test_pdf_upload_logic` and `test_doc_upload_logic`. These functions will contain the logic currently in the `test_pdf_upload` and `test_doc_upload` methods respectively.
    
    Here are the changes:
    
    ```python
    test_app.py
    <<<<<<< ORIGINAL
        @patch('app.st')
        def test_pdf_upload(self, mock_st):
            mock_st.file_uploader.return_value = MagicMock(type='application/pdf')
            with open('example.pdf', 'rb') as f:
                mock_st.file_uploader.return_value.getvalue.return_value = f.read()
            app.main()
            # TODO: Add assertions based on the expected behavior of app.main()
    
        @patch('app.st')
        def test_doc_upload(self, mock_st):
            mock_st.file_uploader.return_value = MagicMock(type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
            with open('example.docx', 'rb') as f:
                mock_st.file_uploader.return_value.getvalue.return_value = f.read()
            app.main()
            # TODO: Add assertions based on the expected behavior of app.main()
    =======
        def test_pdf_upload_logic(self, mock_st):
            mock_st.file_uploader.return_value = MagicMock(type='application/pdf')
            with open('example.pdf', 'rb') as f:
                mock_st.file_uploader.return_value.getvalue.return_value = f.read()
            app.main()
            # TODO: Add assertions based on the expected behavior of app.main()
    
        def test_doc_upload_logic(self, mock_st):
            mock_st.file_uploader.return_value = MagicMock(type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
            with open('example.docx', 'rb') as f:
                mock_st.file_uploader.return_value.getvalue.return_value = f.read()
            app.main()
            # TODO: Add assertions based on the expected behavior of app.main()
    
        @patch('app.st')
        def test_pdf_upload(self, mock_st):
            self.test_pdf_upload_logic(mock_st)
    
        @patch('app.st')
        def test_doc_upload(self, mock_st):
            self.test_doc_upload_logic(mock_st)
    >>>>>>> UPDATED
    ```
    
    This change separates the logic into their own functions, and the original test methods now call these new functions. This makes the code more modular and easier to maintain.

--- test_app.py
+++ test_app.py
@@ -3,21 +3,27 @@ import app
 from unittest.mock import patch, MagicMock
 
 class TestApp(unittest.TestCase):
-    @patch('app.st')
-    def test_pdf_upload(self, mock_st):
+    def test_pdf_upload_logic(self, mock_st):
         mock_st.file_uploader.return_value = MagicMock(type='application/pdf')
         with open('example.pdf', 'rb') as f:
             mock_st.file_uploader.return_value.getvalue.return_value = f.read()
         app.main()
         # TODO: Add assertions based on the expected behavior of app.main()
 
-    @patch('app.st')
-    def test_doc_upload(self, mock_st):
+    def test_doc_upload_logic(self, mock_st):
         mock_st.file_uploader.return_value = MagicMock(type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
         with open('example.docx', 'rb') as f:
             mock_st.file_uploader.return_value.getvalue.return_value = f.read()
         app.main()
         # TODO: Add assertions based on the expected behavior of app.main()
+
+    @patch('app.st')
+    def test_pdf_upload(self, mock_st):
+        self.test_pdf_upload_logic(mock_st)
+
+    @patch('app.st')
+    def test_doc_upload(self, mock_st):
+        self.test_doc_upload_logic(mock_st)
 
 if __name__ == '__main__':
     unittest.main()
